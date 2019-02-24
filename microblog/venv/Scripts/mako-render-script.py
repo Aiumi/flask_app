9de55a8acda4c3b2d32e886d812ac89944f0519f
Aiumi <brandonaiumiyen@gmail.com>
2019-02-23 16:54:24 -0800

    First commit

---
+++ microblog/venv/Scripts/mako-render-script.py
@@ -0,0 +1,12 @@
+#!c:\users\penga_000\desktop\python_projects\flask_app\microblog\venv\scripts\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'Mako==1.0.7','console_scripts','mako-render'
+__requires__ = 'Mako==1.0.7'
+import re
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
+    sys.exit(
+        load_entry_point('Mako==1.0.7', 'console_scripts', 'mako-render')()
+    )
